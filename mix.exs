b8cb0e58c99f7ef9bea13926a9bd4ad89834e53b
c4710n <c4710n@users.noreply.github.com>
2023-04-09 19:39:40 +0800

    chore: prepare for publishing

--- mix.exs
+++ mix.exs
@@ -1,14 +1,24 @@
 defmodule CozyProxy.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Proxy requests to other plugs."
+  @source_url "https://github.com/cozy-elixir/cozy_proxy"
+
   def project do
     [
       app: :cozy_proxy,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: docs(),
+      package: package(),
+      aliases: aliases()
     ]
   end
 
@@ -27,9 +37,37 @@ defmodule CozyProxy.MixProject do
     [
       {:plug, "~> 1.14"},
       {:plug_cowboy, ">= 2.6.0"},
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false},
       {:phoenix, "~> 1.7", only: [:test]},
       {:jason, "~> 1.0", only: [:test]},
       {:websock_adapter, ">= 0.5.0", only: [:test]}
     ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version
+    ]
+  end
+
+  defp package do
+    [
+      exclude_patterns: [],
+      licenses: ["Apache-2.0"],
+      links: %{GitHub: @source_url}
+    ]
+  end
+
+  defp aliases do
+    [publish: ["hex.publish", "tag"], tag: &tag_release/1]
+  end
+
+  defp tag_release(_) do
+    Mix.shell().info("Tagging release as #{@version}")
+    System.cmd("git", ["tag", @version])
+    System.cmd("git", ["push", "--tags"])
   end
 end
