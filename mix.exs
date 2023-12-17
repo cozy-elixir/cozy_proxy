b78d7806edd7a561667bcf1d0297fd4241c1475b
c4710n <c4710n@users.noreply.github.com>
2023-12-17 17:59:06 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozyProxy.MixProject do
   use Mix.Project
 
-  @version "0.2.2"
+  @version "0.2.3"
   @description "Proxy requests to other plugs."
   @source_url "https://github.com/cozy-elixir/cozy_proxy"
 
